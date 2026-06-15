afe61796bc1a5f86600cead47102694be95026be
Lemuel Duarte <153552353+lduarte-dev@users.noreply.github.com>
2026-06-15 01:26:57 -0300

    busca cep

---
+++ busca_cep/next.config.ts
@@ -0,0 +1,12 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  output: "export",
+  reactCompiler: true,
+  images: {
+    unoptimized: true,
+  },
+  basePath: "busca_cep",
+};
+
+export default nextConfig;
